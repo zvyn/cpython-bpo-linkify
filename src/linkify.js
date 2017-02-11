f9ab5c147474d02e7c97cfc8e57bbc6959f6990c
Berker Peksag <berker.peksag@gmail.com>
2017-02-11 09:49:58 +0300

    Check for .commit-desc too

--- src/linkify.js
+++ src/linkify.js
@@ -5,17 +5,22 @@ chrome.extension.sendMessage({}, function(response) {
 
     var regex = /(?:(?:issues?|bugs?|SF)(?:\s+id:?)?\s*#?|#)\s*(\d+)/gmi;
 
-    var commitTitleElm = document.querySelector(".commit-title");
+    function replacer(match, p1, offset, string) {
+      return '<a href="https://bugs.python.org/issue' + p1 + '">' + match + '</a>';
+    }
 
+    var commitTitleElm = document.querySelector(".commit-title");
     if (commitTitleElm !== null) {
       var commitTitle = commitTitleElm.innerHTML.trim();
-
-      function replacer(match, p1, offset, string) {
-        return '<a href="https://bugs.python.org/issue' + p1 + '">' + match + '</a>';
-      }
-
       var newTitle = commitTitle.replace(regex, replacer);
       commitTitleElm.innerHTML = newTitle;
+    }
+
+    var commitDescElm = document.querySelector(".commit-desc");
+    if (commitDescElm !== null) {
+      var commitDesc = commitDescElm.innerHTML.trim();
+      var newDesc = commitDesc.replace(regex, replacer);
+      commitDescElm.innerHTML = newDesc;
     }
   }
   }, 10);
